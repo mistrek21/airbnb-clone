a82ce915b4b979b65a14a25e0bd152ef5f29cff8
mata <mata@matas-MacBook-Air.local>
2021-08-15 19:30:24 +0200

    fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,5 @@
 module.exports = {
   plugins: [
     'tailwindcss',
-    [
-      'postcss-preset-env',
-      {
-        autoprefixer: {
-          flexbox: 'no-2009',
-        },
-        stage: 3,
-        features: {
-          'custom-properties': false,
-        },
-      },
-    ],
   ],
 }
